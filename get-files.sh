cdffe16f0cfa86e48ccc864c885da438bc5edb95
Riccardo Roveri <ricky.roveri@gmail.com>
2022-03-29 17:12:09 +0200

    fix get-files

--- get-files.sh
+++ get-files.sh
@@ -4,5 +4,5 @@ if [[ -d "files" ]]; then
   rm -rf files
 fi
 
-scp -r -P 222 -i /home/rick/.ssh/id_rsa root@192.168.1.220:/volume1/veeam/raspi/ files
+scp -r -P 222 -i /home/rick/.ssh/id_rsa root@srvnas.roveri.local:/volume1/veeam/raspi/ files
 chmod -R 755 files
